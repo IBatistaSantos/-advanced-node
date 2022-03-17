f37c10e2fc65598e69e50437128c4f120da1b6bf
Israel Batista <israelbatiista19@gmail.com>
2022-03-17 18:47:51 -0300

    ensure Service creates new account if LoadUserAccountRepo returns undefined

--- src/data/services/facebook-authentication.ts
+++ src/data/services/facebook-authentication.ts
@@ -1,24 +1,33 @@
 import { AuthenticationError } from '@/domain/errors'
 import { FacebookAuthentication } from '@/domain/features'
 import { LoadFacebookUserApi } from '../contracts/apis'
-import { CreateFacebookAccountRepository, LoadUserAccountRepository } from '../contracts/repos'
+import { CreateFacebookAccountRepository, LoadUserAccountRepository, UpdateFacebookAccountRepository } from '../contracts/repos'
 
 export class FacebookAuthenticationService {
   constructor (
     private readonly facebookApi: LoadFacebookUserApi,
-    private readonly userAccountRepository: LoadUserAccountRepository & CreateFacebookAccountRepository
+    private readonly userAccountRepository: LoadUserAccountRepository & CreateFacebookAccountRepository & UpdateFacebookAccountRepository
   ) {}
 
   async perform (params: FacebookAuthentication.Params): Promise<AuthenticationError> {
     const fbData = await this.facebookApi.loadUser({ token: params.token })
 
     if (fbData !== undefined) {
-      await this.userAccountRepository.load({ email: fbData?.email })
-      await this.userAccountRepository.createFromFacebook({
-        email: fbData.email,
-        name: fbData.name,
-        facebookId: fbData.facebookId
-      })
+      const accountData = await this.userAccountRepository.load({ email: fbData?.email })
+
+      if (accountData !== undefined) {
+        await this.userAccountRepository.updateWithFacebook({
+          id: accountData.id,
+          name: fbData.name,
+          facebookId: fbData.facebookId
+        })
+      } else {
+        await this.userAccountRepository.createFromFacebook({
+          email: fbData.email,
+          name: fbData.name,
+          facebookId: fbData.facebookId
+        })
+      }
     }
     return new AuthenticationError()
   }
