a812fff0d36224d7ccefdc140b372e430414566d
Israel Batista <israelbatiista19@gmail.com>
2022-03-24 10:11:19 -0300

    ensure Service calls TokenGenrator with correct params

--- src/data/services/facebook-authentication.ts
+++ src/data/services/facebook-authentication.ts
@@ -2,12 +2,14 @@ import { AuthenticationError } from '@/domain/errors'
 import { FacebookAuthentication } from '@/domain/features'
 import { FacebookAccount } from '@/domain/models'
 import { LoadFacebookUserApi } from '../contracts/apis'
+import { TokenGenerator } from '../contracts/crypto'
 import { LoadUserAccountRepository, SaveFacebookAccountRepository } from '../contracts/repos'
 
 export class FacebookAuthenticationService {
   constructor (
     private readonly facebookApi: LoadFacebookUserApi,
-    private readonly userAccountRepository: LoadUserAccountRepository & SaveFacebookAccountRepository
+    private readonly userAccountRepository: LoadUserAccountRepository & SaveFacebookAccountRepository,
+    private readonly crypto: TokenGenerator
   ) {}
 
   async perform (params: FacebookAuthentication.Params): Promise<AuthenticationError> {
@@ -17,7 +19,8 @@ export class FacebookAuthenticationService {
       const accountData = await this.userAccountRepository.load({ email: fbData.email })
       const fbAccount = new FacebookAccount(fbData, accountData)
 
-      await this.userAccountRepository.saveWithFacebook(fbAccount)
+      const { id } = await this.userAccountRepository.saveWithFacebook(fbAccount)
+      await this.crypto.generateToken({ key: id })
     }
     return new AuthenticationError()
   }
