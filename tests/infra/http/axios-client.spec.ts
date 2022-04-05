d40968360ca27ced2cdc16dd457bde537d415690
Israel Batista <israelbatiista19@gmail.com>
2022-04-05 15:21:17 -0300

    isolate SUT creation

--- tests/infra/http/axios-client.spec.ts
+++ tests/infra/http/axios-client.spec.ts
@@ -6,30 +6,30 @@ jest.mock('axios')
 
 class AxiosHttpClient {
   async get (args: HttpGetClient.Params): Promise<void> {
-    await axios.get(args.url, {
-      params: args.params
-    })
+    await axios.get(args.url, { params: args.params })
   }
 }
 
 describe('AxiosHttpClient', () => {
+  let sut: AxiosHttpClient
+  let fakeAxios: jest.Mocked<typeof axios>
+  let url: string
+  let params: object
+  beforeAll(() => {
+    url = 'any_url'
+    params = {
+      any_param: 'any_value'
+    }
+    fakeAxios = axios as jest.Mocked<typeof axios>
+  })
+  beforeEach(() => {
+    sut = new AxiosHttpClient()
+  })
   describe('get', () => {
     it('should return a promise', async () => {
-      const fakeAxios = axios as jest.Mocked<typeof axios>
-      const sut = new AxiosHttpClient()
+      await sut.get({ url, params })
 
-      await sut.get({
-        url: 'any_url',
-        params: {
-          any_param: 'any_value'
-        }
-      })
-
-      expect(fakeAxios.get).toHaveBeenCalledWith('any_url', {
-        params: {
-          any_param: 'any_value'
-        }
-      })
+      expect(fakeAxios.get).toHaveBeenCalledWith(url, { params })
 
       expect(fakeAxios.get).toHaveBeenCalledTimes(1)
     })
