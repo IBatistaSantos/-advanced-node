5bb8d5c76aaa94b00c224c3cc5da3846848d6ac3
Israel Batista <israelbatiista19@gmail.com>
2022-05-13 13:01:53 -0300

    move production code to their own files

--- tests/application/controllers/facebook-login.spec.ts
+++ tests/application/controllers/facebook-login.spec.ts
@@ -1,6 +1,9 @@
+import { FacebookLoginController } from '@/application/controllers'
+import { ServerError } from '@/application/errors'
 import { AuthenticationError } from '@/domain/errors'
 import { FacebookAuthentication } from '@/domain/features'
 import { AccessToken } from '@/domain/models'
+
 import { mock, MockProxy } from 'jest-mock-extended'
 
 describe('FacebookLoginController', () => {
@@ -72,50 +75,3 @@ describe('FacebookLoginController', () => {
     })
   })
 })
-
-class FacebookLoginController {
-  constructor (private readonly facebookAuthentication: FacebookAuthentication) {}
-  async handle (httpRequest: any): Promise<HttpResponse> {
-    try {
-      if (httpRequest.token === '' || httpRequest.token === undefined || httpRequest.token === null) {
-        return {
-          statusCode: 400,
-          data: new Error('The field token is required')
-        }
-      }
-      const result = await this.facebookAuthentication.perform({ token: httpRequest.token })
-
-      if (result instanceof AccessToken) {
-        return {
-          statusCode: 200,
-          data: {
-            accessToken: result.value
-          }
-        }
-      } else {
-        return {
-          statusCode: 401,
-          data: result
-        }
-      }
-    } catch (error) {
-      return {
-        statusCode: 500,
-        data: new ServerError(error as Error)
-      }
-    }
-  }
-}
-
-type HttpResponse = {
-  statusCode: number
-  data: any
-}
-
-class ServerError extends Error {
-  constructor (error?: Error) {
-    super('Server failded. Try again soon')
-    this.name = 'ServerError'
-    this.stack = error?.stack
-  }
-}
