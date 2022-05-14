b44b7312ac76b6cc1e528d204609b36cc74fbd92
Israel Batista <israelbatiista19@gmail.com>
2022-05-14 18:26:55 -0300

    move RequiredStringValidator to its own file

--- tests/application/validation/required-string.spec.ts
+++ tests/application/validation/required-string.spec.ts
@@ -1,32 +1,28 @@
 import { RequiredFieldError } from '@/application/errors'
+import { RequiredStringValidator } from '@/application/validation'
 
 describe('RequiredStringValidator', () => {
   it('should return RequiredFieldError if value is empty', () => {
     const sut = new RequiredStringValidator('', 'any_field')
-    const error = sut.validate('')
+    const error = sut.validate()
     expect(error).toEqual(new RequiredFieldError('any_field'))
   })
 
   it('should return RequiredFieldError if value is null', () => {
     const sut = new RequiredStringValidator(null as any, 'any_field')
-    const error = sut.validate('')
+    const error = sut.validate()
     expect(error).toEqual(new RequiredFieldError('any_field'))
   })
 
   it('should return RequiredFieldError if value is undefined', () => {
     const sut = new RequiredStringValidator(undefined as any, 'any_field')
-    const error = sut.validate('')
+    const error = sut.validate()
     expect(error).toEqual(new RequiredFieldError('any_field'))
   })
+
+  it('should return undefined if value is not empty', () => {
+    const sut = new RequiredStringValidator('any_value', 'any_field')
+    const error = sut.validate()
+    expect(error).toBeUndefined()
+  })
 })
-
-class RequiredStringValidator {
-  constructor (
-    private readonly value: string,
-    private readonly fieldName: string
-  ) {}
-
-  validate (value: string): Error | undefined {
-    return new RequiredFieldError('any_field')
-  }
-}
