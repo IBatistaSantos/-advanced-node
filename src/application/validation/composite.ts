e376af34b24bd1acedd3f3ede39e52f989c5357d
Israel Batista <israelbatiista19@gmail.com>
2022-05-16 13:11:52 -0300

    move production code to their own files

---
+++ src/application/validation/composite.ts
@@ -0,0 +1,16 @@
+import { Validator } from './validator'
+
+export class ValidationComposite implements Validator {
+  constructor (private readonly validators: Validator[]) {}
+
+  validate (): undefined | Error {
+    for (const validator of this.validators) {
+      const error = validator.validate()
+
+      if (error !== undefined) {
+        return error
+      }
+    }
+    return undefined
+  }
+}
